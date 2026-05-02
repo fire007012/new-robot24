595ec88d39d1434b5d48ef98919f154be44ae2dd
commonnuc <rera@rera.com>
2026-05-03 05:19:49 +0800

    arm_control: switch gripper commands to velocity mode

--- arm_control/scripts/gripper_cmd_node.py
+++ arm_control/scripts/gripper_cmd_node.py
@@ -2,13 +2,12 @@
 
 import rospy
 from std_msgs.msg import Bool, Float64
-from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 
 class GripperCmdNode(object):
     def __init__(self):
-        self.position_topic = rospy.get_param(
-            "~position_topic", "/arm_control/gripper_position"
+        self.velocity_topic = rospy.get_param(
+            "~velocity_topic", "/arm_control/gripper_velocity"
         )
         self.open_close_topic = rospy.get_param(
             "~open_close_topic", "/arm_control/gripper_open"
@@ -16,16 +15,16 @@ class GripperCmdNode(object):
         self.output_topic = rospy.get_param(
             "~output_topic", "/gripper_controller/command"
         )
-        self.joint_name = rospy.get_param("~joint_name", "left_gripper_finger_joint")
-        self.min_pos = float(rospy.get_param("~min_position", 0.0))
-        self.max_pos = float(rospy.get_param("~max_position", 0.071))
-        self.open_pos = float(rospy.get_param("~open_position", 0.071))
-        self.close_pos = float(rospy.get_param("~close_position", 0.0))
-        self.duration = float(rospy.get_param("~move_duration", 0.8))
+        self.open_velocity = float(rospy.get_param("~open_velocity", 0.8))
+        self.close_velocity = float(rospy.get_param("~close_velocity", -0.8))
+        self.command_hold_sec = float(rospy.get_param("~command_hold_sec", 0.2))
+        self.max_abs_velocity = float(rospy.get_param("~max_abs_velocity", 0.0))
+        self.stop_deadline = None
+        self.timer = rospy.Timer(rospy.Duration(0.02), self.timer_cb)
 
-        self.pub = rospy.Publisher(self.output_topic, JointTrajectory, queue_size=10)
-        self.sub_pos = rospy.Subscriber(
-            self.position_topic, Float64, self.pos_cb, queue_size=10
+        self.pub = rospy.Publisher(self.output_topic, Float64, queue_size=10)
+        self.sub_vel = rospy.Subscriber(
+            self.velocity_topic, Float64, self.vel_cb, queue_size=10
         )
         self.sub_open = rospy.Subscriber(
             self.open_close_topic, Bool, self.open_cb, queue_size=10
@@ -33,33 +32,47 @@ class GripperCmdNode(object):
 
         rospy.loginfo(
             "gripper_cmd_node started: %s -> %s",
-            self.position_topic,
+            self.velocity_topic,
             self.output_topic,
         )
         rospy.loginfo(
             "gripper_cmd_node open/close topic: %s", self.open_close_topic
         )
+        rospy.loginfo(
+            "gripper_cmd_node bool velocities: open=%.3f close=%.3f hold=%.3fs",
+            self.open_velocity,
+            self.close_velocity,
+            self.command_hold_sec,
+        )
 
-    def clamp(self, val):
-        return max(self.min_pos, min(self.max_pos, val))
+    def normalize_velocity(self, value):
+        velocity = float(value)
+        if self.max_abs_velocity > 0.0:
+            velocity = max(-self.max_abs_velocity, min(self.max_abs_velocity, velocity))
+        return velocity
 
-    def publish_target(self, target):
-        traj = JointTrajectory()
-        traj.header.stamp = rospy.Time.now()
-        traj.joint_names = [self.joint_name]
+    def publish_velocity(self, velocity):
+        self.pub.publish(Float64(data=self.normalize_velocity(velocity)))
 
-        point = JointTrajectoryPoint()
-        point.positions = [self.clamp(target)]
-        point.time_from_start = rospy.Duration.from_sec(self.duration)
-
-        traj.points = [point]
-        self.pub.publish(traj)
-
-    def pos_cb(self, msg):
-        self.publish_target(msg.data)
+    def vel_cb(self, msg):
+        self.stop_deadline = None
+        self.publish_velocity(msg.data)
 
     def open_cb(self, msg):
-        self.publish_target(self.open_pos if msg.data else self.close_pos)
+        velocity = self.open_velocity if msg.data else self.close_velocity
+        self.publish_velocity(velocity)
+        if self.command_hold_sec > 0.0:
+            self.stop_deadline = rospy.Time.now() + rospy.Duration.from_sec(self.command_hold_sec)
+        else:
+            self.stop_deadline = None
+
+    def timer_cb(self, _event):
+        if self.stop_deadline is None:
+            return
+        if rospy.Time.now() < self.stop_deadline:
+            return
+        self.stop_deadline = None
+        self.publish_velocity(0.0)
 
 
 if __name__ == "__main__":
